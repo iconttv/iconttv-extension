abc7b8e7b07a68c738aa63802d14544425914f76
unknown <k123s456h@gmail.com>
2023-05-01 18:22:08 +0900

    feat: add version_name

--- pack.js
+++ pack.js
@@ -1,6 +1,6 @@
 const { existsSync, mkdirSync } = require('fs');
 const AdmZip = require('adm-zip');
-const VERSION = require('./src/version');
+const {VERSION, VERSION_NAME} = require('./src/version');
 
 const APP_NAME = 'iconttv';
 const isFirefox = process.env.BROWSER_ENV === 'firefox';
@@ -8,8 +8,8 @@ const isFirefox = process.env.BROWSER_ENV === 'firefox';
 try {
   const outdir = 'release';
   const filename = isFirefox
-    ? `${APP_NAME}-firefox-v${VERSION}.zip`
-    : `${APP_NAME}-chrome-v${VERSION}.zip`;
+    ? `${APP_NAME}-firefox-v${VERSION}-${VERSION_NAME}.zip`
+    : `${APP_NAME}-chrome-v${VERSION}-${VERSION_NAME}.zip`;
   const zip = new AdmZip();
   zip.addLocalFolder('build');
   if (!existsSync(outdir)) {
